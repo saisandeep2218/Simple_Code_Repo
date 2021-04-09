7d6032adaa13f4b08a98634c809903cc164e1125
saisandeep2218 <65825124+saisandeep2218@users.noreply.github.com>
2021-04-09 18:41:57 +0530

    First Commit-01

---
+++ src/test/java/runner/Executor.java
@@ -0,0 +1,11 @@
+package runner;
+
+import testScript.Maths_Add;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		         Maths_Add.maths_addition();
+	}
+
+}
---
+++ src/test/java/testScript/Maths_Add.java
@@ -0,0 +1,18 @@
+package testScript;
+
+public class Maths_Add {
+	
+	
+	           public static void maths_addition() {
+	        	    try {
+	        	    	 int  a = 10;
+	        	    	 int  b = 25;
+	        	    	 int  c = a+b;
+	        	    	 System.out.println("The Value of C is --> " + c);	        	    		 
+	        	    }
+	        	    catch(Exception E) {
+	        	    	E.printStackTrace();
+	        	    }
+	           }
+
+}
